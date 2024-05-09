33d0d470e4f3581a6a7cd8e20759283b28c26000
akntheartisan <arvindnagarj.n@gmail.com>
2024-05-09 18:24:05 +0530

    jwt token created

--- Models/userSchema.js
+++ Models/userSchema.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 const validator = require('validator');
+const bcrypt = require('bcrypt');
 
 const userschema = new schema({
   name: {
@@ -29,6 +30,13 @@ const userschema = new schema({
     },
     message: 'password is not same',
   },
+});
+
+userschema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  this.confirmPassword = undefined;
+  next();
 });
 
 module.exports = mongoose.model('user', userschema);
--- controller/authController.js
+++ controller/authController.js
@@ -1,22 +1,28 @@
 const userModel = require('../Models/userSchema');
+const jwt = require('jsonwebtoken');
 
-exports.signup = async (req,res,next)=>{
+exports.signup = async (req, res, next) => {
+  try {
+    const newUser = await userModel.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword,
+    });
 
-    try {
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_Secret, {
+      expiresIn: process.env.JWT_Expiration,
+    });
 
-        const newUser = await userModel.create(req.body);
-        res.status(200).json({
-            status:'success',
-            data:{user:newUser}
-        });
-        
-    } catch (error) {
-
-        res.status(400).json({
-            status:'fail',
-            message:error
-          });
-        
-    }
-
-}
+    res.status(200).json({
+      status: 'success',
+      token,
+      data: { user: newUser },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error,
+    });
+  }
+};
